3abb3d39e23a144bc6ee0d6b53ae68527337485e
BG00924 <BG00924@users.noreply.github.com>
2020-07-19 12:08:25 -0500

    completed first fetch request and population of data to the webpage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,72 @@
-var searchInput = document.getElementById('gamesubmit')
+var searchInputBtn = document.getElementById('gamesubmit')
+var searchInputEl = document.getElementById('search-Input')
+var platformDropdownEl = document.getElementById('platform-dropdown')
+var nameEl = document.getElementById('name')
+var platformEl = document.getElementById('platform')
+var genreEl = document.getElementById('genre')
+var scoreEl = document.getElementById('score')
+var descriptionEl = document.getElementById('description')
+//var platformDropdown = document.getElementById('platformtype')
+var gameImageEl = document.getElementById('gameimage')
 
+
+// API fetch from Chicken Coop which provides the game information
+var getGameInfo = function(game, platform) {
+    fetch("https://chicken-coop.p.rapidapi.com/games/" + game + "?platform=" + platform, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "chicken-coop.p.rapidapi.com",
+		"x-rapidapi-key": "2dced9ba08msh3b63096614e89a5p18b302jsnd75e5b94f864"
+	}
+    })
+        .then(response => {
+        //console.log(response);
+        if (response.ok) {
+            response.json().then(data => {
+                //console.log(data)
+                displayGameInfo(data, game, platform)
+            })
+        }
+        })
+        .catch(err => {
+	    console.log(err);
+        });
+}
+// End Chicken Coop fetch
+
+// Displays the information to the website
+var displayGameInfo = function (game, searchTerm) {
+    nameEl.textContent = ""
+    nameEl.textContent = game.result.title
+    platformEl.textContent = ""
+    platformEl.textContent = platformDropdownEl.value
+    genreEl.textContent = ""
+    for (var i = 0; i < game.result.genre.length; i++) {
+        genreEl.textContent = game.result.genre[i]
+    }
+    scoreEl.textContent = ""
+    scoreEl.textContent = game.result.score
+    descriptionEl.textContent = ""
+    descriptionEl.textContent = game.result.description
+    gameImageEl.setAttribute("src", "")
+    gameImageEl.setAttribute("src", game.result.image)
+}
+// End display function
+
+// Function that handles what happens when game name and platform submitted
 var searchSubmitHandler = function(event) {
     event.preventDefault();
-    grabGame();
-
-
+    var gameInput = searchInputEl.value.trim()
+    var platformInput = platformDropdownEl.value.trim()
+    if (gameInput && platformInput)
+        getGameInfo(gameInput, platformInput)
+        searchInputEl.value = ""
+        //platformDropdownEl.value = ""
+        grabGame();
 }
+// End submit handler
 
+// Loads items saved to local storage
 
 var gamenamels = localStorage.getItem("Game Name");
 // console log variable gamenamels
@@ -23,10 +83,12 @@ var platformls = localStorage.getItem("platformdropdown");
 // get search input and console log it
 // put searchinput into #name
 
+// End load saved items
+
 // this function runs when it is called on click of the submit button in the index.html file
 function grabGame() {
     // this variable is = to the value of the element with id="searchInput"
-    var searchInput = document.getElementById("search-Input").value;
+    var searchInput = nameEl.value.trim();
     // console log variable searchInput
     // console.log("searchinupt", searchInput);
     // this sets the inner html of the element with id="name" to the variable searchInput
@@ -35,11 +97,11 @@ function grabGame() {
     localStorage.setItem("Game Name", searchInput);
 
 
-    var platformdropdown = document.getElementById("platform-dropdown").value;
+    var platformdropdown = platformEl.value.trim();
     // this variable is = to the value of the element with id="platform-dropdown"
     // console.log("platform-dropdown", platformdropdown);
     // console log variable platformdropdown
-    document.getElementById("platform").innerHTML = platformdropdown;
+    //document.getElementById("platform").innerHTML = platformdropdown;
     // this sets the inner html of the element with id="platform" to the variable platformdropdown
     localStorage.setItem("platformdropdown", platformdropdown);
     // this sets local storage with the key="platformdropdown" and value equal to the variable platformdropdown
@@ -48,5 +110,7 @@ function grabGame() {
 };
 
 
-searchInput.addEventListener("submit", searchSubmitHandler);
+// Event listeners
+searchInputBtn.addEventListener("click", searchSubmitHandler);
 
+//getGameInfo("overwatch", "pc")
